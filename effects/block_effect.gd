c2a2cf10a9f044a1b82cf91cf7f2bae24383bcbb
Lkarm003 <lkarm003@ucr.edu>
2025-05-06 19:01:55 -0700

    Update block_effect.gd

--- effects/block_effect.gd
+++ effects/block_effect.gd
@@ -2,7 +2,16 @@ class_name BlockEffect
 extends Effect
 
 var amount: int = 0
-
+#Test
+# Goal: make sure that it increases block for each valid target 
+#func test_block_does_it_work()
+	# var target = Enemy.new()
+	# target.stats = Stats.new()
+	# target.stats.block = 2;
+	# var effect = BlockEffect.new()
+	# effect.amount = 4
+	# effect.execute([target])
+	# assert_eq(target.stats.block, 6, "Block should increase by 4")
 
 func execute(targets: Array[Node]) -> void:
 	for target in targets:
