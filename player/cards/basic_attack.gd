8830cb44b3e5a5cf97a378f26b715499163d0477
Lkarm003 <lkarm003@ucr.edu>
2025-05-06 19:26:03 -0700

    update test cases for basic_attack and basic_block

--- player/cards/basic_attack.gd
+++ player/cards/basic_attack.gd
@@ -1,6 +1,16 @@
 extends Card
 
-
+#Create a test to see if the basic attack works
+#test
+# func test_basic_attack_does_it_work()
+	# var target = Enemy.new()
+	# target.stats = Stats.new()
+	# target.stats.health = 5
+	# var choose_card = card.new()
+	# choose_card.apply_effects([target])
+	# assert_eq(target.stats.health, 3, "Health should decrease by 2 after function is run")
+	
+	
 func apply_effects(targets: Array[Node]) -> void:
 	var damage_effect := DamageEffect.new()
 	damage_effect.amount = 2
--- player/cards/basic_block.gd
+++ player/cards/basic_block.gd
@@ -1,6 +1,16 @@
 extends Card
 
 
+#Create a test to see if the basic block works
+#test
+# func test_basic_attack_does_it_work()
+	# var target = Enemy.new()
+	# target.stats = Stats.new()
+	# target.stats.hlock= 5
+	# var choose_card = card.new()
+	# choose_card.apply_effects([target])
+	# assert_eq(target.stats.block, 8, "Block should increase by 3 after the function is run")
+	
 func apply_effects(targets: Array[Node]) -> void:
 	var block_effect := BlockEffect.new()
 	block_effect.amount = 3
