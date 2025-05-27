6c84d33e170c3b5fe2744f95e828a325716198ef
AnthonyDuong5 <aduon067@ucr.edu>
2025-05-26 17:20:39 -0700

    Simplify

--- Scenes/ui/intent_ui.gd
+++ Scenes/ui/intent_ui.gd
@@ -2,8 +2,6 @@ class_name IntentUI
 extends HBoxContainer
 
 @onready var icon: TextureRect = $Icon
-@onready var number: Label = $Number
-
 
 func update_intent(intent: Intent) -> void:
 		if not intent:
@@ -12,7 +10,5 @@ func update_intent(intent: Intent) -> void:
 		
 		icon.texture = intent.icon
 		icon.visible = icon.texture != null
-		number.text = str(intent.number)
-		number.visible = intent.number.length() > 0
 		show()
 	
