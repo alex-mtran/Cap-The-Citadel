68dc586fc96ae5b2d58797adad5c4055bb2fe9bb
alex-mtran <alex-mtran@yahoo.com>
2025-06-04 19:54:33 -0700

    Add campfire. Add health to topbar

--- ex/campfire/campfire.gd
+++ ex/campfire/campfire.gd
@@ -1,4 +1,10 @@
+class_name Campfire
 extends Control
+
+@export var char_stats: CharacterStats
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 
 func _ready() -> void:
 	if not MainMusic.playing:
@@ -7,5 +13,10 @@ func _ready() -> void:
 	if BattleMusic.playing:
 		BattleMusic.stop()
 
-func _on_button_pressed() -> void:
+func _on_rest_button_pressed() -> void:
+	char_stats.heal(ceili(char_stats.max_health * 0.3))
+	animation_player.play("fade_out")
+
+# Called from the AnimationPlayer at the end of 'fade-out'
+func _on_fade_out_finished() -> void:
 	Events.campfire_exited.emit()
